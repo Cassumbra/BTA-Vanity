887d93910a194d01b17a05c1d61b64e261ebc3cb
Cassumbra <penumbruh@gmail.com>
2023-04-09 17:41:51 -0500

    disable armor

--- src/main/java/varigata/vanity/mixin/DisableArmor.java
+++ src/main/java/varigata/vanity/mixin/DisableArmor.java
@@ -1,4 +1,23 @@
 package varigata.vanity.mixin;
 
+import net.minecraft.src.EntityPlayer;
+import net.minecraft.src.RenderPlayer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = RenderPlayer.class, remap = false)
 public class DisableArmor {
+
+    public boolean hideArmor = false;
+
+    @Inject(method = "setArmorModel", at = @At(value = "HEAD"), cancellable = true)
+    void vanity_setArmorModel(EntityPlayer entityplayer, int i, float f, CallbackInfoReturnable<Boolean> cir) {
+        if (hideArmor) {
+            cir.setReturnValue(false);
+            return;
+        }
+
+    }
 }
