4a7c2d0bd7ec3878b6c13a32868057806f87923a
Cassumbra <penumbruh@gmail.com>
2023-04-10 17:38:46 -0500

    make button appear in proper place in creative

--- src/main/java/varigata/vanity/mixin/vanity_GuiInventoryCreative.java
+++ src/main/java/varigata/vanity/mixin/vanity_GuiInventoryCreative.java
@@ -1,4 +1,39 @@
 package varigata.vanity.mixin;
 
-public class vanity_GuiInventoryCreative {
+
+import net.minecraft.src.GuiButton;
+import net.minecraft.src.GuiInventoryCreative;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(value = GuiInventoryCreative.class, remap = false)
+public class vanity_GuiInventoryCreative extends vanity_GuiInventory {
+
+    int hideArmorButtonFloatX = 117;
+
+    @Override
+    void vanity_initGui(CallbackInfo ci) {
+        this.hideArmorButton = new GuiButton(101, this.width / 2 - hideArmorButtonFloatX, this.height / 2 - 74, 9, 9, "");
+        this.hideArmorButton.visible = false;
+        //boolean enableArmorButton = false;
+
+        //for(int i = 0; i < this.mc.thePlayer.inventory.getSizeInventory(); ++i) {
+        //    ItemStack itemStack = this.mc.thePlayer.inventory.getStackInSlot(i);
+        //    if (itemStack != null && itemStack.getItem() instanceof ItemArmor) {
+        //        enableArmorButton = true;
+        //        break;
+        //    }
+        //}
+
+        //if (enableArmorButton) {
+        //this.hideArmorButton = new GuiButton(101, this.width / 2 - this.armourButtonFloatX, this.height / 2 - 74, 9, 9, "");
+        //this.hideArmorButton.visible = false;
+        this.controlList.add(this.hideArmorButton);
+        //} else {
+        //    this.hideArmorButton = null;
+        //}
+
+
+    }
+
 }
